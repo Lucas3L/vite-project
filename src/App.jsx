a43e17524e064410453c1a7978210d415598a846
Lucas3L <lucasllindemann@hotmail.com>
2023-09-08 14:16:30 -0300

    Aplicação de Map
    Uso map para imprimir arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
 import './App.css'
 
+const users = [
+  {
+    name: 'Carlos',
+    address: 'Rua X',
+    age: 28,
+    isAdmin: false,
+  },
+  {
+    name: 'Maria',
+    address: 'Rua XX',
+    age: 31,
+    isAdmin: true,
+  },
+  {
+    name: 'Matheus',
+    address: 'Rua XI',
+    age: 22,
+    isAdmin: false,
+  }
+]
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>React router dom</h1>
+      <div>
+        {users.map((users) => (
+          <div>Cliente: {users.name}. Mora em: {users.address}.
+          Idade: {users.age}.</div>
+        ))}
+      </div>
     </>
   )
 }
